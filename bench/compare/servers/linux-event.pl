693298ba3a8fb84bb3c94adb5e8e721cdf581f40
Joshua S. Day <haxmeister@hotmail.com>
2026-09-20 18:35:35 -0500

    Instrument native Stream counters during benchmark

--- bench/compare/servers/linux-event.pl
+++ bench/compare/servers/linux-event.pl
@@ -3,11 +3,15 @@ use strict;
 use warnings;
 
 use Linux::Event::Loop;
+use Linux::Event::Kernel::Timer;
 use Linux::Event::WebSocket::Server;
 
 my $port = $ENV{PORT} // 9101;
 my $mode = $ENV{BENCH_MODE} // 'echo';
 my $ack = '{"ok":true}';
+my $collect_stats = $ENV{BENCH_STATS} // 0;
+my @connections;
+my $stats_timer;
 
 my $loop = Linux::Event::Loop->new;
 my $server = Linux::Event::WebSocket::Server->new(
@@ -15,6 +19,42 @@ my $server = Linux::Event::WebSocket::Server->new(
     host => '127.0.0.1',
     port => $port,
     max_message_size => 32 * 1024 * 1024,
+
+    on_open => sub ($ws) {
+        return if !$collect_stats;
+        push @connections, $ws;
+        return if $stats_timer;
+
+        $stats_timer = Linux::Event::Kernel::Timer->new(
+            loop => $loop,
+            after => 1.5,
+            on_timer => sub ($timer) {
+                my %sum;
+                my $active = 0;
+                for my $connection (@connections) {
+                    my $state = $connection->{xs_state} or next;
+                    my $stats = $state->stats;
+                    ++$active;
+                    $sum{$_} += $stats->{$_} // 0 for qw(
+                        consumer_input_calls read_calls bytes_read
+                        input_appends write_submit_calls write_calls writev_calls
+                        bytes_written queued_segments write_eagain_count
+                        pending_bytes
+                    );
+                }
+                warn join(' ',
+                    'BENCH_STATS',
+                    "active=$active",
+                    map { "$_=" . ($sum{$_} // 0) } qw(
+                        consumer_input_calls read_calls bytes_read
+                        input_appends write_submit_calls write_calls writev_calls
+                        bytes_written queued_segments write_eagain_count
+                        pending_bytes
+                    ),
+                ) . "\n";
+            },
+        );
+    },
 
     on_message => sub ($ws, $payload, $type) {
         if ($mode eq 'application') {
