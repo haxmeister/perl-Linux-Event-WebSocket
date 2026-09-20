6bc723cf8d82452734600537da04c7ce3a6a426f
Joshua S. Day <haxmeister@hotmail.com>
2026-09-19 19:55:58 -0500

    Expose raw consumer ABI definition

--- lib/Linux/Event/WebSocket/_BQ.pm
+++ lib/Linux/Event/WebSocket/_BQ.pm
@@ -11,4 +11,14 @@ XSLoader::load(
     $Linux::Event::WebSocket::VERSION,
 );
 
+sub raw_consumer_definition ($class) {
+    return {
+        provider           => \&_raw_consumer_operations_address,
+        abi_version        => 1,
+        operations_address => _raw_consumer_operations_address(),
+    };
+}
+
+sub CLONE_SKIP { 1 }
+
 1;
