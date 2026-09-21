9ad45b89a8a1d4f0af62ba5a88082575cdeccca6
Joshua S. Day <haxmeister@hotmail.com>
2026-09-20 20:41:39 -0500

    Version public connection package for 0.001

--- lib/Linux/Event/WebSocket/Server/Connection.pm
+++ lib/Linux/Event/WebSocket/Server/Connection.pm
@@ -3,6 +3,8 @@ use v5.36;
 use strict;
 use warnings;
 
+our $VERSION = '0.001';
+
 use parent 'Linux::Event::WebSocket::Connection';
 
 # Linux::Event::HTTP keeps its constructor-installed transport-close wrapper
